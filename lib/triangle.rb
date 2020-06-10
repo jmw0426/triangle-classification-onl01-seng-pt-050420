5f4f2ba1e19100cc180e1c7502113b2b1392c478
Joshua Williams <jmw0426@hotmail.com>
2020-06-10 03:40:37 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize
+    
+  end
+  
+  def kind 
+    
+  end 
+  
+    class TriangleError < StandardError 
+      
+    end
 end
